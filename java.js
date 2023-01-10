96b99ced126b3bde67efcd4f08e6f6c2c03a1314
Joe Carrado <jojocarrado@gmail.com>
2023-01-09 19:50:04 -0500

    make game algorithm

---
+++ java.js
@@ -0,0 +1,29 @@
+function getComputerChoice() {
+let computerChoices = ["rock", "paper", "scissors"];
+let random =  Math.floor((Math.random() * computerChoices.length)); //multiplies random decimal b/t 0 and 1 by array length (3), then rounds down to whole number
+let computerSelection = computerChoices[random]; //uses whole number form previous line (1, 2 or 3) to pick string from array with corresponding position
+return computerSelection
+}
+
+
+
+
+function game (playerSelection, computerSelection) {
+
+        if (playerSelection === computerSelection) {
+            return "That's a tie!"
+        } else if (playerSelection === "rock" && computerSelection === "paper") {
+            return "You lose"
+        } else if (playerSelection === "rock" && computerSelection === "scissors") {
+            return "You win!"
+        } else if (playerSelection === "paper" && computerSelection === "rock") {
+            return "You win!"
+        } else if (playerSelection === "paper" && computerSelection === "scissors") {
+            return "You lose"
+        } else if (playerSelection === "scissors" && computerSelection === "rock") {
+            return "You lose"
+        } else if (playerSelection === "scissors" && computerSelection === "paper") {
+            return "You win!"
+    }}
+
+let computerSelection = getComputerChoice()
